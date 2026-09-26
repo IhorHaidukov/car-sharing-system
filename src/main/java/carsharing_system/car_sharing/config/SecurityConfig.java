d52e72c4482b3ef1327fee79c8ab6886d9db57f3
IgorHaidukov <kolyhool9@gmail.com>
2026-09-26 18:46:56 +0200

    Configure stateless JWT security

--- src/main/java/carsharing_system/car_sharing/config/SecurityConfig.java
+++ src/main/java/carsharing_system/car_sharing/config/SecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 @Configuration
 @RequiredArgsConstructor
@@ -21,6 +22,9 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(csrf -> csrf.disable())
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
                 .authorizeHttpRequests(auth -> auth
 
                                 .requestMatchers(
