f34a591bd95ec1593e70750afd504c673cbf6105
IgorHaidukov <kolyhool9@gmail.com>
2026-09-26 18:00:17 +0200

    Validate login request

--- src/main/java/carsharing_system/car_sharing/dto/LoginRequestDto.java
+++ src/main/java/carsharing_system/car_sharing/dto/LoginRequestDto.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 
 @Getter
 @Setter
@@ -11,6 +13,11 @@ import lombok.Setter;
 @AllArgsConstructor
 public class LoginRequestDto {
 
+
+    @NotBlank(message = "Email cannot be empty")
+    @Email(message = "Invalid email format")
     private String email;
+
+    @NotBlank(message = "Password cannot be empty")
     private String password;
 }
